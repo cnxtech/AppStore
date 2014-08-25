0ec4b08b72417ac7202f40fd08e08c49d0bc10bc
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2014-08-25 12:27:36 +0200

    Updated the data model + admin

--- appstore/appstoreitems/models.py
+++ appstore/appstoreitems/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django_extensions.db.fields import UUIDField
+
 
 class Plattform(models.Model):
     name = models.CharField(max_length=30)
@@ -10,8 +12,26 @@ class AppImage(models.Model):
     image = models.ImageField()
 
 
+class ApiVersion(models.Model):
+    version_no = models.CharField(max_length=10)
+
+
+class Version(models.Model):
+    api_version = models.ManyToManyField(ApiVersion)
+    changelog = models.TextField()
+
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField(max_length=200, blank=True)
+
+
 class AppStoreItem(models.Model):
+    vendor = models.ForeignKey(Vendor)
     name = models.CharField(max_length=100)
     desc = models.TextField()
     images = models.ManyToManyField(AppImage)
     platforms = models.ManyToManyField(Plattform)
+    versions = models.ManyToManyField(Version)
+    uuid = UUIDField()
+    url = models.URLField(max_length=200)
