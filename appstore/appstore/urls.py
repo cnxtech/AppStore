977f0bcb002a8e885e0228e5c01ea69b9148cdbb
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2014-09-19 16:04:44 +0200

    Fixed root url that is stays only on /

--- appstore/appstore/urls.py
+++ appstore/appstore/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns(
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('webstore.urls')),
+    url(r'^', include('webstore.urls')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
