8496902c8727e649584d30e04f9ca9e8c91b0645
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2014-09-25 14:49:50 +0200

    Pass the app obj in the App View

--- appstore/webstore/views.py
+++ appstore/webstore/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from appstoreitems.models import AppStoreItem
+
 
 class ListView(TemplateView):
     template_name = "webstore/app-list.html"
@@ -7,3 +9,10 @@ class ListView(TemplateView):
 
 class AppView(TemplateView):
     template_name = "webstore/app.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(AppView, self).get_context_data(**kwargs)
+        uuid = self.request.GET.get("id")
+        context['app'] = AppStoreItem.objects.filter(uuid=uuid).get()
+        return context
